8d1e2400599722a7c6a027c14c6c12f45bb1cb04
DavidXT <David.xt.nguyen@gmail.com>
2022-11-30 14:59:15 +0100

    add index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :games
+  resources :index
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "games#index"
+  root "index#index"
 end
