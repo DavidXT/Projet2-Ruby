aa6152e810af2cdbd0a21f1b3cd5729e27354506
Sathariel <leochabert@free.fr>
2022-11-30 17:33:48 +0100

    Mod, new nav bar

--- db/schema.rb
+++ db/schema.rb
@@ -15,47 +15,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_160625) do
   enable_extension "adminpack"
   enable_extension "plpgsql"
 
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.bigint "resource_id"
-    t.string "author_type"
-    t.bigint "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
-  end
-
-  create_table "admin_users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at", precision: nil
-    t.datetime "remember_created_at", precision: nil
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admin_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "category_posts", force: :cascade do |t|
-    t.bigint "category_id", null: false
-    t.bigint "post_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_category_posts_on_category_id"
-    t.index ["post_id"], name: "index_category_posts_on_post_id"
-  end
-
   create_table "clients", force: :cascade do |t|
     t.string "Name"
     t.string "Email"
@@ -72,16 +31,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_160625) do
     t.integer "like"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "picture"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "name"
-    t.string "content"
-    t.integer "reading_time"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "category"
   end
 
   create_table "prestations", force: :cascade do |t|
@@ -92,18 +41,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_160625) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at", precision: nil
-    t.datetime "remember_created_at", precision: nil
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  add_foreign_key "category_posts", "categories"
-  add_foreign_key "category_posts", "posts"
 end
